71d3279af95f919477f49250307ba1692bd30734
rc-earnest <rc.earnest04@gmail.com>
2025-01-30 08:28:59 -0700

    good do loop examples

--- doLoops/doLoops.vb
+++ doLoops/doLoops.vb
@@ -5,7 +5,35 @@ Option Compare Text
 Module doLoops
 
     Sub Main()
+        Dim firstNumber As Integer
 
+        Do
+            Console.WriteLine("In the first loop. Loop while firstNumber <=3")
+            firstNumber += 1
+            Console.WriteLine($"FirstNumber is {firstNumber}!")
+            Console.WriteLine()
+        Loop While firstNumber <= 3
+
+        Do
+            Console.WriteLine("In the second loop. Loop while firstNumber <=3")
+            firstNumber += 1
+            Console.WriteLine($"FirstNumber is {firstNumber}!")
+            Console.WriteLine()
+        Loop While firstNumber <= 3
+
+        Do Until firstNumber >= 3
+            Console.WriteLine("In the third loop. Do until firstNumber >=3")
+            firstNumber += 1
+            Console.WriteLine($"FirstNumber is {firstNumber}!")
+            Console.WriteLine()
+        Loop
+
+        Do While firstNumber >= 3
+            Console.WriteLine("In the fourth loop. Do while firstNumber >=3")
+            firstNumber -= 1
+            Console.WriteLine($"FirstNumber is {firstNumber}!")
+            Console.WriteLine()
+        Loop
     End Sub
 
 End Module
