9ab6e3c12feea3d929c756420131467ea2ebaf15
rc-earnest <rc.earnest04@gmail.com>
2025-01-30 08:56:15 -0700

    start program flow, user interaction

--- doLoops/doLoops.vb
+++ doLoops/doLoops.vb
@@ -6,34 +6,62 @@ Module doLoops
 
     Sub Main()
         Dim firstNumber As Integer
+        Dim userInput As String
+
+        'Do
+        '    Console.WriteLine("In the first loop. Loop while firstNumber <=3")
+        '    firstNumber += 1
+        '    Console.WriteLine($"FirstNumber is {firstNumber}!")
+        '    Console.WriteLine()
+        'Loop While firstNumber <= 3
+
+        'Do
+        '    Console.WriteLine("In the second loop. Loop while firstNumber <=3")
+        '    firstNumber += 1
+        '    Console.WriteLine($"FirstNumber is {firstNumber}!")
+        '    Console.WriteLine()
+        'Loop While firstNumber <= 3
+
+        'Do Until firstNumber >= 3
+        '    Console.WriteLine("In the third loop. Do until firstNumber >=3")
+        '    firstNumber += 1
+        '    Console.WriteLine($"FirstNumber is {firstNumber}!")
+        '    Console.WriteLine()
+        'Loop
+
+        'Do While firstNumber >= 3
+        '    Console.WriteLine("In the fourth loop. Do while firstNumber >=3")
+        '    firstNumber -= 1
+        '    Console.WriteLine($"FirstNumber is {firstNumber}!")
+        '    Console.WriteLine()
+        'Loop
 
         Do
-            Console.WriteLine("In the first loop. Loop while firstNumber <=3")
-            firstNumber += 1
-            Console.WriteLine($"FirstNumber is {firstNumber}!")
-            Console.WriteLine()
-        Loop While firstNumber <= 3
+            Console.WriteLine("Enter a number")
+            Console.WriteLine("Enter Q to quit")
+            userInput = Console.ReadLine()
+            Console.WriteLine($"you entered {userInput}")
 
-        Do
-            Console.WriteLine("In the second loop. Loop while firstNumber <=3")
-            firstNumber += 1
-            Console.WriteLine($"FirstNumber is {firstNumber}!")
-            Console.WriteLine()
-        Loop While firstNumber <= 3
+            Try
+                firstNumber = CInt(userInput)
+            Catch ex As Exception
+                If userInput <> "Q" Then
+                    Console.WriteLine($"{userInput} Is not a whole number!")
+                End If
+            End Try
 
-        Do Until firstNumber >= 3
-            Console.WriteLine("In the third loop. Do until firstNumber >=3")
-            firstNumber += 1
-            Console.WriteLine($"FirstNumber is {firstNumber}!")
-            Console.WriteLine()
-        Loop
 
-        Do While firstNumber >= 3
-            Console.WriteLine("In the fourth loop. Do while firstNumber >=3")
-            firstNumber -= 1
-            Console.WriteLine($"FirstNumber is {firstNumber}!")
-            Console.WriteLine()
-        Loop
+
+
+
+
+
+
+
+
+
+            Console.Clear()
+        Loop While userInput <> "Q"
     End Sub
 
 End Module
